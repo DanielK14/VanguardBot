83256edd4747b7146e473cf2f62d36db10ed8c5d
Daniel Krois <danielkrois@hotmail.com>
2022-04-08 15:09:57 +0200

    added twitter command to bot

--- VanguardBot/Program.cs
+++ VanguardBot/Program.cs
@@ -1,28 +1,67 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Discord;
+using Discord.Net;
 using Discord.WebSocket;
+using Newtonsoft.Json;
 
 namespace VanguardBot
 {
     internal class Program
     {
         private DiscordSocketClient _client;
+        readonly ulong guildID = ulong.Parse(Environment.GetEnvironmentVariable("guildID"));
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            new Program().MainAsync().GetAwaiter().GetResult();
         }
         public async Task MainAsync()
         {
             _client = new DiscordSocketClient();
-            //_client.Log += Log;
+            _client.Log += Log;
             await _client.LoginAsync(TokenType.Bot,
                 Environment.GetEnvironmentVariable("DiscordToken"));
             await _client.StartAsync();
-            ulong id = ulong.Parse(Environment.GetEnvironmentVariable("DiscordChannel"));
+            _client.Ready += Client_Ready;
+            _client.SlashCommandExecuted += SlashCommandHandler;
+            
 
             await Task.Delay(-1); //needed to keep the program open until you manually stop the program
         }
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+        public async Task Client_Ready()
+        {
+            var statusCommand = new SlashCommandBuilder();
+            statusCommand.WithName("status");
+            statusCommand.WithDescription("Get the latest tweet from BungieHelp twitter.");
 
+            try
+            {
+                await _client.Rest.CreateGuildCommand(statusCommand.Build(), guildID);
+            }
+            catch (HttpException exception)
+            {
+                // If our command was invalid, we should catch an ApplicationCommandException. This exception contains the path of the error as well as the error message. You can serialize the Error field in the exception to get a visual of where your error is.
+                var json = JsonConvert.SerializeObject(exception.Errors, Formatting.Indented);
+                Console.WriteLine(json);
+            }
+        }
+        private async Task SlashCommandHandler(SocketSlashCommand command)
+        {
+            switch (command.Data.Name)
+            {
+                case "status":
+                    new TwitterMethods().TwitterMain(command);
+                    break;
+                default:
+                    await command.RespondAsync($"You have run {command.Data.Name}, which does nothing.");
+                    break;
+            }
+
+        }
     }
 }
---
+++ VanguardBot/TwitterMethods.cs
@@ -0,0 +1,27 @@
+﻿using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitterSharp;
+
+namespace VanguardBot
+{
+    class TwitterMethods
+    {
+        readonly string twitterToken = Environment.GetEnvironmentVariable("BearerToken");
+        private TwitterSharp.Client.TwitterClient _client;
+
+        public async void TwitterMain(SocketSlashCommand command)
+        {
+            _client = new TwitterSharp.Client.TwitterClient(twitterToken);
+            var bungieHelp = await _client.GetUserAsync("BungieHelp");
+            var answer = await _client.GetTweetsFromUserIdAsync(bungieHelp.Id);
+            var tweet = answer[0];
+            await command.RespondAsync(tweet.Text);           
+
+        }
+
+    }
+}
