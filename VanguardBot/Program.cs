05c26956ed7c43d78e82bc1c2120cd1358d1d1c4
Daniel Krois <danielkrois@hotmail.com>
2022-04-08 11:17:20 +0200

    initial commit

---
+++ VanguardBot/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+
+namespace VanguardBot
+{
+    internal class Program
+    {
+        private DiscordSocketClient _client;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public async Task MainAsync()
+        {
+            _client = new DiscordSocketClient();
+            //_client.Log += Log;
+            await _client.LoginAsync(TokenType.Bot,
+                Environment.GetEnvironmentVariable("DiscordToken"));
+            await _client.StartAsync();
+            ulong id = ulong.Parse(Environment.GetEnvironmentVariable("DiscordChannel"));
+
+            await Task.Delay(-1); //needed to keep the program open until you manually stop the program
+        }
+
+    }
+}
